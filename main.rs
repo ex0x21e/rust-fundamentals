6bc9dea245fde70622e1465fee25ee493c038e5c
ex0x21e <119272433+ex0x21e@users.noreply.github.com>
2026-07-16 14:53:31 +1200

    done

--- main.rs
+++ main.rs
@@ -1,17 +1,6 @@
-// The Read trait allows for reading bytes from a source.
-// Implementors of the Read trait are called ‘readers’.
-use std::io::Read;
-
 fn main() {
-    //Rectangle Area
-    let mut input = String::new();
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
 
-    std::io::stdin().read_to_string(&mut input).unwrap();
-
-    let mut it = input.split_whitespace();
-
-    let width: i64 = it.next().unwrap().parse().unwrap();
-    let height: i64 = it.next().unwrap().parse().unwrap();
-
-    println!("{:.2}", width * height);
+    println!("{}", buf.to_uppercase())
 }
