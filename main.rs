be81e0b54f79277c029e3734d47d72098c888dc7
ex0x21e <119272433+ex0x21e@users.noreply.github.com>
2026-07-16 16:42:17 +1200

    done

--- main.rs
+++ main.rs
@@ -1,6 +1,11 @@
-fn main() {
-    let mut buf = String::new();
-    std::io::stdin().read_line(&mut buf).unwrap();
+use std::io::{self, BufRead};
 
-    println!("{}", buf.to_uppercase())
+fn main() {
+    let stdin = io::stdin();
+    let mut iter = stdin.lock().lines();
+    let name = iter.next().unwrap().unwrap();
+    let age: i32 = iter.next().unwrap().unwrap().trim().parse().unwrap();
+    // Print the greeting.
+    let _ = (&name, age);
+    println!("Hi, {}! You are {} years old.", name, age);
 }
