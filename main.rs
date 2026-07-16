ee27a07f247bfe7004477882de932140b68a4302
ex0x21e <119272433+ex0x21e@users.noreply.github.com>
2026-07-16 13:41:06 +1200

    done

--- main.rs
+++ main.rs
@@ -1,3 +1,17 @@
+// The Read trait allows for reading bytes from a source.
+// Implementors of the Read trait are called ‘readers’.
+use std::io::Read;
+
 fn main() {
-    println!("Hello, Rust!");
+    let mut input = String::new();
+    std::io::stdin()
+    .read_to_string(& mut input) //reads all bytes and converts to string
+    .unwrap();
+
+    let mut it = input.split_whitespace();
+
+    let a: i64 = it.next().unwrap().parse().unwrap();
+    let b: i64 = it.next().unwrap().parse().unwrap();
+
+    println!("{}", a+b);
 }
